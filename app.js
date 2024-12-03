5a538d18fc767ebf572f445568e0e206ef53c8c9
USHINDI BIHAME Victoire <157381633+ub-victor@users.noreply.github.com>
2024-12-03 08:18:00 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// DOM Manipulation 
+
+// const title = document.querySelector("#main-heading");
+
+// title.style.color = 'red';
+
+//console.log(title);
+
+
+//styling using DOM
+
+/* const listItems = document.querySelectorAll(".list-items");
+
+for(i = 0; i < listItems.length; i++){
+    listItems[i].style.fontSize = '2rem';
+}
+console.log(listItems); */
+
+
+// Create Element 
+
+const ul = document.querySelector('ul');
+const li = document.createElement('li');
+
+// Adding Element
+
+ul.append(li);
+
+
+//Modifying
+
+li.innerText = 'X-ment' 
