6d9167a0c6470511f0729b0d54c239ac711973fd
ub-victor <victoireushindi371@gmail.com>
2025-05-27 23:53:05 +0200

    Initial commit

--- app.js
+++ app.js
@@ -32,4 +32,4 @@
 // li.innerText = 'X-ment' 
 
 
-console.log(document););
+console.log(document); // this Examine the document object by showing everything we can be able to do with it like creating elements, modifying elements, etc.
