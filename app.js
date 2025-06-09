10e6522e62af2f688e7e283e654eaa6dc950bb53
ub-victor <victoireushindi371@gmail.com>
2025-06-09 23:57:15 +0200

    Initial commit

--- app.js
+++ app.js
@@ -69,14 +69,29 @@
 
 // GETELEMENTBYCLASSNAME
 
-var items = document.getElementsByClassName('list-group-item')
-console.log(items[0])
-items[1].textContent = "Hello 2"
-items[1].style.fontWeight = 'bold'
-items[1].style.backgroundColor = 'yellow'
+// var items = document.getElementsByClassName('list-group-item')
+// console.log(items[0])
+// items[1].textContent = "Hello 2"
+// items[1].style.fontWeight = 'bold'
+// items[1].style.backgroundColor = 'yellow'
+
+// //items.style.backgroundColor = '#f4f4f4'; //so here this will not gonna work it will prompt an error as it is a html collection / an array let loop over it so that we can edit or style each item
+
+// for(let i = 0; i< items.length; i++){
+//     items[i].style.backgroundColor = 'orange';
+// }
+
+
+//GETELEMENTBYTAGNAME//
+
+var li = document.getElementsByTagName('li')
+console.log(li)
+li[1].textContent = "Hello 2"
+li[1].style.fontWeight = 'bold'
+li[1].style.backgroundColor = 'yellow'
 
 //items.style.backgroundColor = '#f4f4f4'; //so here this will not gonna work it will prompt an error as it is a html collection / an array let loop over it so that we can edit or style each item
 
-for(let i = 0; i< items.length; i++){
-    items[i].style.fontWeight = 'bold';
+for(let i = 0; i< li.length; i++){
+    li[i].style.backgroundColor = 'orange';
 }
