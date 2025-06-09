015f44e7b8340c83ef7f16ef19598648dcedfcde
ub-victor <victoireushindi371@gmail.com>
2025-06-10 01:10:13 +0200

    Initial commit

--- app.js
+++ app.js
@@ -158,7 +158,6 @@ var  title = document.querySelectorAll('.title');
 console.log(title);
 
 var odd = document.querySelectorAll('li:nth-child(odd)')
-odd.style.backgroundColor = 'orange';
-// for(var i = 0; i< odd.length; i++){
-	
-// }
+for(var i = 0; i< odd.length; i++){
+	odd[i].style.backgroundColor = 'orange';
+}
