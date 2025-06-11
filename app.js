991711dce5d1b6c82703d6fcf28d5e7703951122
ub-victor <victoireushindi371@gmail.com>
2025-06-11 11:30:57 +0200

    Initial commit

--- app.js
+++ app.js
@@ -261,7 +261,10 @@ console.log(newDiv)
 
 // let change the front size
 
+newDiv.style.fontSize = '30px';
 newDiv.style.fontSize = '30px'
+
+
 
 container.insertBefore(newDiv, h1) // here we are inserting newDiv before h1
 
