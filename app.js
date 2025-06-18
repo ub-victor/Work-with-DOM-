1db50d9c9b1314d12ab34b21d50bf11b5f19d583
ub-victor <victoireushindi371@gmail.com>
2025-06-18 13:27:32 +0200

    Initial commit

--- app.js
+++ app.js
@@ -338,6 +338,10 @@ var box = document.getElementById('box')
 
 var itemInput = document.querySelector('input[type="text"]')
 
+var form = document.querySelector('form')
+
+itemInput.addEventListener('keydown', runEvent);
+
 function runEvent(e){
     console.log('EVENT TYPE: '+ e.type );
 
