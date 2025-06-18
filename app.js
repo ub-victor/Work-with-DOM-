0cfa952485af57b32faa93763054a260d4739d51
ub-victor <victoireushindi371@gmail.com>
2025-06-18 20:37:17 +0200

    Initial commit

--- app.js
+++ app.js
@@ -368,6 +368,7 @@ function runEvent(e){
     console.log('EVENT TYPE: '+ e.type );
     // Log the current value of the input field to the console
     console.log(e.target.value.length); 
+    document.body.style.display ='none' // evoid cheat
     // document.getElementById('output').innerHTML = '<h3> Your name is: '+e.target.value+'<h3/>';
 
     /**
